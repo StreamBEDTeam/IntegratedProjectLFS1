c85cc6ea1767bc4513ca8194ca72abb03ee58897
Alina <alina.striner@gmail.com>
2019-02-03 06:36:17 -0500

    added M scenes, worked on summary board

--- VR/Assets/SummaryItem.cs
+++ VR/Assets/SummaryItem.cs
@@ -8,8 +8,9 @@ public class SummaryItem : MonoBehaviour
     public RectTransform Bar;
     public SummaryCount Count;
     public string FeatureName;
-    public float widthPerCount=10f;
-    public int tagcount;
+    public float maxwidth=10f;
+    public int tagcount=0;
+    public int maxcount=0;
 
     private GameStateHandle gameState;
     //TextAlignment 
@@ -22,8 +23,9 @@ public class SummaryItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //tagcount = gameState.Instance.getTagCount(FeatureName);
-        //Bar.localScale = new Vector3(tagcount * widthPerCount, Bar.localScale.y, Bar.localScale.z);
+        tagcount = gameState.Instance.getTagCount(FeatureName);
+        int max = maxcount > 1 ? maxcount : 1;
+        float widthPerCount = maxwidth / max;
         Bar.sizeDelta = new Vector2(tagcount * widthPerCount, Bar.sizeDelta.y);
         Count.Text.text = string.Format("{0}", tagcount);
         Count.RectTransform.localPosition = new Vector3(tagcount * widthPerCount, 0, 0);
