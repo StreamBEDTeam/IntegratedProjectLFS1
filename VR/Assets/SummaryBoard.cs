a0cca23a96c1313832f6cf3c753bcad30c9b2833
Alina <alina.striner@gmail.com>
2019-02-05 23:21:50 -0500

    Added .unity file back, also committed all unversioned files from several commits

---
+++ VR/Assets/SummaryBoard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummaryBoard : MonoBehaviour
+{
+    SummaryItem[] items;
+    public float maxWidth=10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        items = GetComponentsInChildren<SummaryItem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int max = 0;
+        foreach(var item in items)
+        {
+            if(item.tagcount > max)
+            {
+                max = item.tagcount;
+            }
+        }
+        foreach(var item in items)
+        {
+            item.maxcount = max;
+            item.maxwidth = maxWidth;
+        }
+    }
+}
