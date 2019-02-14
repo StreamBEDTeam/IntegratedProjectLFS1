af437d1f89561ec2a854749ad64ab5e773a39a68
algol001 <alina.goldman@gmail.com>
2019-02-14 18:21:03 -0500

    Add files via upload
    added scene controller from aashrey and added tour controller changes

---
+++ VR/Assets/Scripts/SceneComposer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class SceneComposer : MonoBehaviour
+{
+
+    private ArduinoSerial arduinoSerial = new ArduinoSerial();
+
+    static string SCENE1_CONFIG = "Hello World";
+    static string SCENE2_CONFIG = "";
+    static string SCENE3_CONFIG = "";
+    static string SCENE4_CONFIG = "";
+    static string SCENE5_CONFIG = "";
+    static string SCENE6_CONFIG = "";
+    static string SCENE7_CONFIG = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arduinoSerial.ConnectToArduino();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onSceneLoaded(string sceneName)
+    {
+        Debug.Log("SceneComposer: Loaded new scene: " + sceneName);
+        if (sceneName == "Scene1")
+        {
+            arduinoSerial.SendText(SCENE1_CONFIG);
+        }
+        else if (sceneName == "Scene2")
+        {
+            arduinoSerial.SendText(SCENE2_CONFIG);
+        }
+        
+    }
+}
+
--- VR/Assets/Scripts/TourController.cs
+++ VR/Assets/Scripts/TourController.cs
@@ -1,20 +1,42 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TourController : MonoBehaviour
 {
 
+    private SceneComposer sceneComposer;
+
     public GameObject cameraPanel;
+
+    public void Start()
+    {
+        sceneComposer = new SceneComposer();    
+    }
 
     public void setCameraMode(bool isEnabled)
     {
-        Debug.Log("Setting camera mode: " + isEnabled);
         cameraPanel.SetActive(isEnabled);
     }
 
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    IEnumerator LoadYourAsyncScene(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        while (!asyncLoad.isDone)
+        {
+            onSceneLoaded(sceneName);
+            yield return null;
+        }
+    }
+
+    private void onSceneLoaded(string sceneName)
+    {
+        sceneComposer.onSceneLoaded(sceneName);
     }
 
 }
